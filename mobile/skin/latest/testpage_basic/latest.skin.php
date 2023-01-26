a425bf42fe673723985f2d275e9dc237153cd4f0
dkagh012 <qowjdxo00@gmail.com>
2023-01-27 00:02:04 +0900

    testpage 작업 종료

--- mobile/skin/latest/testpage_basic/latest.skin.php
+++ mobile/skin/latest/testpage_basic/latest.skin.php
@@ -9,13 +9,10 @@ $thumb_width = 500;
 $thumb_height = 350;
 ?>
 <div class="hit_box">
-  <div class="lt box">
-    <div class="latest_hit_box">
-      <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_title"><strong>베스트글</strong></a>
-      <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_more"><span class="sound_only">
-          <?php echo $bo_subject ?>
-        </span>전체보기</a>
-    </div>
+  <div class="latest_hit_box">
+    <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_title"><strong>실시간 베스트</strong></a>
+  </div>
+  <div class="lt_box">
     <ul>
       <?php
       for ($i = 0; $i < count($list); $i++) {
@@ -27,44 +24,44 @@ $thumb_height = 350;
         }
         ?>
         <li>
-
-          <?php
-          //echo $list[$i]['icon_reply']." ";
-          echo "<a href=\"" . $list[$i]['href'] . "\" class=\"lt_tit\">";
-          if ($list[$i]['icon_secret'])
-            echo "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i> ";
-          if ($list[$i]['is_notice'])
-            echo "<strong>" . $list[$i]['subject'] . "</strong>";
-          else
-            echo $list[$i]['subject'];
-
-          // if ($list[$i]['link']['count']) { echo "[{$list[$i]['link']['count']}]"; }
-          // if ($list[$i]['file']['count']) { echo "<{$list[$i]['file']['count']}>"; }
-        
-
-          echo "</a>";
-
-          ?>
           <div class="lt_info">
-            <div class="lt_info_name">
-              <?php echo $list[$i]['ca_name'] ?>
+            <div>
+              <?php echo "<a href=\"" . $list[$i]['href'] . "\" >"; ?>
+              <p>[
+                <?php echo $list[$i]['ca_name'] ?>]
+              </p>
+              <p>
+                <?php echo $list[$i]['subject']; ?>
+              </p>
             </div>
+            <?php
+
+
+            if ($list[$i]['icon_secret'])
+              echo "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i> ";
+            if ($list[$i]['is_notice'])
+              echo "<strong>" . $list[$i]['subject'] . "</strong>";
+            else
+
+
+
+              echo "</a>";
+
+            ?>
+
+
             <div class="lt_info_title">
-              <span class="sound_only">작성자</span>
-
-
-
-
-              <span class="lt_info_view"><i class="xi-eye-o xi-1x" aria-hidden="true"></i>
-                <?php echo $list[$i]['wr_hit'] ?>
-              </span>
-              <?php if ($list[$i]['comment_cnt']) { ?>
-                <span class="sound_only">댓글</span><i class="fa fa-commenting-o" aria-hidden="true"></i>
-                <?php echo $list[$i]['comment_cnt']; ?><span class="sound_only">개</span><?php } ?>
-              <span class="lt_info_date"><i class="xi-time-o x1-1x" aria-hidden="true"></i>
-                <?php echo $list[$i]['datetime'] ?>
-              </span>
-
+              <p>
+                <span class="lt_info_view"><i class="xi-thumbs-up xi-1x" aria-hidden="true"></i>
+                  <?php echo $list[$i]['wr_good'] ?>
+                </span>
+              </p>
+              <p>
+                <?php if ($list[$i]['comment_cnt']) { ?>
+                  <span class="sound_only">댓글</span><i class="xi-speech-o xi-1x" aria-hidden="true"></i>
+                  <?php echo $list[$i]['comment_cnt']; ?><span class="sound_only">개</span>
+                <?php } ?>
+              </p>
             </div>
           </div>
         </li>
