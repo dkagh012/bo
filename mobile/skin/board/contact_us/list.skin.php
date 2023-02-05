29b7dd648fb4da30d78c3efa770058766382a188
dkagh012 <qowjdxo00@gmail.com>
2023-02-05 16:44:30 +0900

    js 수정

--- mobile/skin/board/contact_us/list.skin.php
+++ mobile/skin/board/contact_us/list.skin.php
@@ -184,88 +184,147 @@
       text-align: center;
       color: #888888;
     }
+
+    .desc_padding {
+      display: none;
+      padding-top: 50px;
+    }
+
+    .Cotact_desc {
+      text-align: right;
+      padding-bottom: 24px;
+    }
+
+    .email_form {
+      display: none;
+    }
+
+    .Cotact_desc>h1 {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 18px;
+      color: var(--colorLogo);
+      line-height: 18px;
+    }
   </style>
 </head>
 
 <body>
-  <div class="mail_form">
-    <div class="mail_tit">
-      <h1>문의하기</h1>
-    </div>
-    <form name="contactform" method="post" action="send.php">
-      <div>
-        <div>
-          <div class="mainTit">
-            <label>이메일 주소</label>
-          </div>
-          <div class="email">
-            <input name="email" type="text" class="ipt" size="30"
-              style="width:100%; height:50px; background:#040B11;border:none; color:#fff;padding: 15px 12px;margin-bottom: 9px;font-size:16px;"
-              maxlength="80" placeholder="답변 받으실 이메일 주소를 입력해주세요" onblur="this.placeholder='답변 받으실 이메일 주소를 입력해주세요'"
-              required>
-          </div>
-        </div>
-
+  <div class="mail_Container">
+    <div class="mail_form">
+      <div class="mail_tit">
+        <h1>문의하기</h1>
+      </div>
+      <form name="contactform" method="post" action="send.php">
         <div>
           <div>
-            <label class="mainTit">유형</label>
+            <div class="mainTit">
+              <label>이메일 주소</label>
+            </div>
+            <div class="email">
+              <input name="email" type="email" class="ipt" size="30" id="email"
+                style="width:100%; height:50px; background:#040B11;border:none; color:#fff;padding: 15px 12px;margin-bottom: 9px;font-size:16px;"
+                maxlength="80" placeholder="답변 받으실 이메일 주소를 입력해주세요" onblur="this.placeholder='답변 받으실 이메일 주소를 입력해주세요'"
+                required>
+              <!-- <button onclick="check_email()">확인</button> -->
+            </div>
+            <div class="Cotact_desc email_form">
+              <h1>이메일을 확인해주세요</h1>
+            </div>
           </div>
-          <div class="toggleform">
-            <select name="toggleform" id="" style="width:100%" required>
-              <option value="문의 유형" disabled selected style="display:none;">문의 유형</option>
-              <option class="toggleItem" value="광고 문의">광고 문의</option>
-              <option class="toggleItem" value="이용 문의">이용 문의</option>
-              <option class="toggleItem" value="오류 신고">오류 신고</option>
-              <option class="toggleItem" value="계정 문의">계정 문의</option>
-              <option class="toggleItem" value="건의 사항">건의 사항</option>
-              <option class="toggleItem" value="기타">기타</option>
-            </select>
-          </div>
-        </div>
 
-        <div class="comments_box">
-          <div scope="row">
-            <label class="mainTit">문의내용</label>
+          <div>
+            <div>
+              <label class="mainTit">유형</label>
+            </div>
+            <div class="toggleform">
+              <select name="toggleform" id="" style="width:100%" required>
+                <option value="문의 유형" disabled selected style="display:none;">문의 유형</option>
+                <option class="toggleItem" value="광고 문의">광고 문의</option>
+                <option class="toggleItem" value="이용 문의">이용 문의</option>
+                <option class="toggleItem" value="오류 신고">오류 신고</option>
+                <option class="toggleItem" value="계정 문의">계정 문의</option>
+                <option class="toggleItem" value="건의 사항">건의 사항</option>
+                <option class="toggleItem" value="기타">기타</option>
+              </select>
+            </div>
           </div>
-          <div valign="bottom" class="comments_down">
-            <textarea onkeyup="counter(this,3000)" class="comment_text" name="comments" cols="50" rows="10"
-              maxlength="3000"
-              style="width:100%; height:208px; background:#040B11;border:none; color:#fff;    padding: 12px 12px 0px 12px;font-size:16px;"
-              required></textarea>
-            <p id="Comment_Count">0 / 3000</p>
-          </div>
-        </div>
 
-        <div class="CommentArea hide">
-          <div class="CommentContents">
-            <h1 class="CommentTIT">문의가 정상적으로 접수되었습니다</h1>
-            <h1 class="CommentTITTE">빠르게 답변을 드리도록 하겠습니다</h1>
-            <input type="submit" value="확인" class="CommentBTN">
+          <div class="comments_box">
+            <div scope="row">
+              <label class="mainTit">문의내용</label>
+            </div>
+            <div valign="bottom" class="comments_down">
+              <textarea onkeyup="counter(this,3000)" class="comment_text" name="comments" cols="50" rows="10"
+                maxlength="3000" id="comment_text"
+                style="width:100%; height:208px; background:#040B11;border:none; color:#fff;    padding: 12px 12px 0px 12px;font-size:16px;"
+                required></textarea>
+              <p id="Comment_Count">0 / 3000</p>
+            </div>
+            <div class="Cotact_desc desc_padding">
+              <h1>내용 파악을 위해 30자 이상을 작성해 주세요</h1>
+            </div>
           </div>
+          <div>
+            <input type="submit" value="확인" class="commnet_BTN">
+          </div>
+
+        </div>
+      </form>
+
+      <div class="CommentArea hide">
+        <div class="CommentContents">
+          <h1 class="CommentTIT">문의가 정상적으로 접수되었습니다</h1>
+          <h1 class="CommentTITTE">빠르게 답변을 드리도록 하겠습니다</h1>
+          <button type="button" class="CommentBTN">제출하기</button>
         </div>
       </div>
-    </form>
-    <div>
-      <button type="button" class="commnet_BTN">제출하기</button>
-    </div>
-    <div class="Comment_lower">
-      <h1>고객센터 운영시간: 평일 9:00 - 18:00 (주말 · 공휴일 제외)</h1>
-      <p>휴일을 제외한 평일에는 빠른 답변을 드리겠습니다<br>
-        오래 기다려도 답변이 오지 않으면 스팸 메일함을 확인해주세요</p>
+      <div class="Comment_lower">
+        <h1>고객센터 운영시간: 평일 9:00 - 18:00 (주말 · 공휴일 제외)</h1>
+        <p>휴일을 제외한 평일에는 빠른 답변을 드리겠습니다<br>
+          오래 기다려도 답변이 오지 않으면 스팸 메일함을 확인해주세요</p>
+      </div>
     </div>
   </div>
   <script>
+    let textarea = document.getElementById('comment_text');
+
+
+    textarea.addEventListener('keypress', () => {
+      const textarea_Box = document.querySelector('.desc_padding');
+      if (textarea.value.length <= 30) {
+        textarea_Box.style.display = 'block';
+      } else if (textarea.value.length > 30) {
+        textarea_Box.style.display = 'none';
+      }
+    })
+
+    // 이메일 내용
+    let emailtest = document.getElementById("email");
+    emailtest.addEventListener('keypress', () => {
+      let input = document.getElementById("email").value;
+      const TE = document.querySelector('.email_form');
+      let email_format = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+      if (email_format.test(input))
+        TE.style.display = 'none';
+      else {
+        TE.style.display = 'block';
+      }
+    })
+
+
+    // 텍스트 글자 갯수
     function counter(text, length) {
       var limit = length;
       var str = text.value.length;
       if (str > limit) {
-        document.getElementById("Comment_Count").innerHTML = "1500자 이상 입력 했습니다";
+        document.getElementById("Comment_Count").innerHTML = "3000자 이상 입력 했습니다";
         text.focus();
       }
       document.getElementById("Comment_Count").innerHTML = text.value.length + "/" + limit;
     }
 
-
+    // 아래 버튼 클릭
     function popupToggle(toggleBtn, area) {
       if (document.querySelector(`.${toggleBtn}`) !== null) {
         const BTN = document.querySelector(`.${toggleBtn}`)
